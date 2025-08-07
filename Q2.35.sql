0fd33ed8155c16b6408f89bf3de1685f26d1d770
rexx010 <grexx200@gmail.com>
2025-08-07 23:53:06 +0100

    Create Q2.35.sql

---
+++ Q2.35.sql
@@ -0,0 +1,1 @@
+The result the count and the sum built in function provide are different, the count function counts the number of all the rows while the sum function sum all the value of the column.
