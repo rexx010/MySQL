c0fff837e008be81eb572eac1e635b36ace39ff2
rexx010 <grexx200@gmail.com>
2025-08-08 15:50:10 +0100

    Delete Q2.34.sql

--- Q2.34.sql
+++
@@ -1,7 +0,0 @@
-SELECT SUM(QuantityOnHand) AS OrderItemSum,
- AVG(QuantityOnHand) AS OrderItemAvg,
- MIN(QuantityOnHand) AS OrderItemMin,
- MAX(QuantityOnHand) AS OrderItemMax,
-COUNT(*) AS NumberOfRows 
-from inventory
-
